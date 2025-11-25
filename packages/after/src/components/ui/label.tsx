a38c79527bcefe19a3f73b8bfed21c9d5dcd0c39
dasom <ds92ko@gmail.com>
2025-11-26 05:38:22 +0900

    feat: 라벨 컴포넌트 스토리 추가

--- packages/after/src/components/ui/label.tsx
+++ packages/after/src/components/ui/label.tsx
@@ -1,9 +1,8 @@
-import * as LabelPrimitive from '@radix-ui/react-label';
-import * as React from 'react';
-
 import { cn } from '@/lib/utils';
+import * as LabelPrimitive from '@radix-ui/react-label';
+import type { ComponentProps } from 'react';
 
-function Label({ className, ...props }: React.ComponentProps<typeof LabelPrimitive.Root>) {
+function Label({ className, ...props }: ComponentProps<typeof LabelPrimitive.Root>) {
   return (
     <LabelPrimitive.Root
       data-slot="label"
