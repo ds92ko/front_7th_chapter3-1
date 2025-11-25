4cacd4698796f698e3090abf52b15730647cc7f4
dasom <ds92ko@gmail.com>
2025-11-26 00:09:34 +0900

    chore: storybook 설치 및 세팅

---
+++ packages/after/.storybook/preview.ts
@@ -0,0 +1,24 @@
+import { withThemeByClassName } from '@storybook/addon-themes';
+import type { Preview } from '@storybook/react-vite';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: '',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+    }),
+  ],
+};
+
+export default preview;
