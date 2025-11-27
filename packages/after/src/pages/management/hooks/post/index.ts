ecf5b09b336853350ef41e3976b6414feb9851a8
dasom <ds92ko@gmail.com>
2025-11-28 08:47:56 +0900

    refactor: barrel exports 추가 및 import 경로 정리

---
+++ packages/after/src/pages/management/hooks/post/index.ts
@@ -0,0 +1,7 @@
+export { usePostCRUD, type PostFormData } from './usePostCRUD';
+export { usePostData } from './usePostData';
+export { usePostDialog } from './usePostDialog';
+export { usePostForm } from './usePostForm';
+export { usePostStats } from './usePostStats';
+export { usePostTable } from './usePostTable';
+
---
+++ packages/after/src/pages/management/hooks/user/index.ts
@@ -0,0 +1,7 @@
+export { useUserCRUD, type UserFormData } from './useUserCRUD';
+export { useUserData } from './useUserData';
+export { useUserDialog } from './useUserDialog';
+export { useUserForm } from './useUserForm';
+export { useUserStats } from './useUserStats';
+export { useUserTable } from './useUserTable';
+
